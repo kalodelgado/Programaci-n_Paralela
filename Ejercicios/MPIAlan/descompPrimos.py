54ec99cd3074ea673b28a405824765e537cd98d5
kalodelgado <30915769+kalodelgado@users.noreply.github.com>
2019-11-14 08:43:29 -0600

    Modificacion en descopPrimos

--- Ejercicios/MPIAlan/descompPrimos.py
+++ Ejercicios/MPIAlan/descompPrimos.py
@@ -68,12 +68,13 @@ def main():
         msj = str(i) + ": "
         factores = descomp_prima(i, l_primos)
 
-        for j in range( len(factores) ):
-            cfactor = factores.count(factores[j]) #cantidad de elementos iguales a f[j]
-            msj += str(factores[j]) + "(" + str(cfactor) + ")"
-            if j != len(factores) - 1:
+        while len(factores) > 0:
+            cantf = factores.count(factores[0])
+            msj += str(factores[0]) + "(" + str(cantf) + ")"
+            for j in range(cantf):
+                factores.remove(factores[0])
+            if len(factores) > 0:
                 msj += " * "
-            j += cfactor
         my_datos.append(msj)
             
     comm.Barrier()
